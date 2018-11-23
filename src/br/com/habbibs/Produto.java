bf218eb267366b065f1c8e84b4569510a9a4341b
Aline Morais <aline.morais27@etec.sp.gov.br>
2018-11-23 20:10:10 -0200

    aaa

---
+++ src/br/com/habbibs/Produto.java
@@ -0,0 +1,27 @@
+package br.com.habbibs;
+
+public class Produto {
+/*Toda classe será iniciado em Uppercase e a string deverá ser sempre simples e lowercase 
+    
+*/    
+    public String nome;
+    public double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+    
+}
